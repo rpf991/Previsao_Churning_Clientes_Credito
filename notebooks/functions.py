f66fca8e40998414d0dcf7e9cdece02d63bfe058
rpf991 <rpf9743@gmail.com>
2026-04-20 20:02:25 -0300

    adaptacao_coeficiente_gini

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,6 +1,10 @@
 from sklearn.metrics import roc_auc_score
 
 def gini_coeficient(y_test, y_pred_probs):
+    """
+    Cálculo do Coeficiente de Gini
+    """
     area_under_reciver = roc_auc_score(y_test, y_pred_probs)
     print(f"Area Under Reciver: {area_under_reciver}")
+
     return 2 * area_under_reciver - 1
