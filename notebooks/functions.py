e012a654e5aaca7b7d2d246cb25a4adab80c7817
rpf991 <rpf9743@gmail.com>
2026-04-23 08:27:35 -0300

    ajuste_texto

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,13 +1,13 @@
 from sklearn.metrics import roc_auc_score
 
-def gini_coeficient(model, X_test, Y_Test):
+def gini_coeficient(model, X_test, Y_Test, classe=1):
     """
     Cálculo do Coeficiente de Gini
     """
 
     # Obtenção de Probabilidades (importante para o Gini)
-    # retorna a chance do cliente ser 1 (Attrited Customer)
-    probs = model.predict_proba(X_test)[:, 1]
+    # retorna a chance do cliente ser 'Attrited Customer'
+    probs = model.predict_proba(X_test)[:, classe]
 
     area_under_reciver = roc_auc_score(Y_Test, probs)
     print(f"Area Under Reciver: {area_under_reciver}")
