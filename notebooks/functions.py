d2e2db60eb50b1bfbd2b18823425035add3fb061
rpf991 <rpf9743@gmail.com>
2026-05-15 20:36:40 -0300

    inclusao CallibredClassifier

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def gini_coeficient(model, X_test, Y_Test, classe=1):
+def gini_coeficient(model, X_test, Y_Test):
     """
     Cálculo do Coeficiente de Gini
     """
-
     # Obtenção de Probabilidades (importante para o Gini)
     # retorna a chance do cliente ser 'Attrited Customer'
-    y_probs = model.predict_proba(X_test)[:, classe]
+    y_probs = model.predict_proba(X_test)[:, 1]
 
     area_under_reciver = roc_auc_score(Y_Test, y_probs)
     print(f"Area Under Reciver: {area_under_reciver}")
 
-    return (2 * area_under_reciver) - 1
+    gini = 2 * area_under_reciver - 1
+
+    return gini
 
 
 def lorenz_curve(Y_test, Y_prob):
     """
     Definição da Curva de Lorenz
     """
-
     df_lorenz = pd.DataFrame({"real": Y_test, "prob": Y_prob}).sort_values(by='prob', ascending=False)
 
     df_lorenz['cum_churn'] = df_lorenz['real'].cumsum()/df_lorenz['real'].sum()
@@ -37,5 +37,4 @@ def lorenz_curve(Y_test, Y_prob):
     plt.legend()
     plt.title("Gráfico de Lorenz")
     plt.show()
-
     
