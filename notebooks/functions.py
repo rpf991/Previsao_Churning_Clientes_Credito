ae3eef71523e24c34220e5390d246612faf8ba87
rpf991 <rpf9743@gmail.com>
2026-06-14 07:57:33 -0300

    ajuste_gmeans_gini_coeficient

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,11 +13,11 @@ def gini_coeficient(model, X_test, Y_Test):
     y_probs = model.predict_proba(X_test)[:, 1]
 
     area_under_reciver = roc_auc_score(Y_Test, y_probs)
-    print(f"Area Under Reciver: {area_under_reciver}")
+    #print(f"Area Under Reciver: {area_under_reciver}")
     
     gini = 2 * area_under_reciver - 1
 
-    return gini
+    return area_under_reciver, gini
 
 
 def lorenz_curve(Y_test, Y_prob):
@@ -37,4 +37,28 @@ def lorenz_curve(Y_test, Y_prob):
     plt.legend()
     plt.title("Gráfico de Lorenz")
     plt.show()
-    
+
+
+def gmean_max(model, X_test, Y_test):
+    """
+    O G-men avalia o equilibrio entre quem vai sair e quem vai entrar 
+
+    TRP -> True Positive Rate (mede a proporção entre positivos reais que foram previstos corretamente no modelo)
+    FRP -> False Positive Rate (mede a proporção entre negativos reais que foram previstos corretamente no modelo)
+    """
+    # 1) Obtenção das probabilidades da classe 1
+    y_probs = model.predict_proba(X_test)[:, 1]
+
+    # 2) Calculo da curva de ROC (grafico usado para avaliar os desempenhos dos modelos de classificação binária)
+    frp, trp, thresholds = roc_curve(Y_test, y_probs)
+
+    # 3) Calculo do G-Mean para cada threshold
+    gmeans = np.sqrt(trp * (1-frp))
+
+    # 4) Encontrar o indice do maior gmean
+    idx = np.argmax(gmeans)
+    limite_ideal = thresholds[idx]
+
+    print(f'Melhor G-Means: {gmeans[idx]}')
+
+    return limite_ideal
